e8ffdb5c7172b9d92c17ee4cf53affc2a842c624
kezialasma <kezia.lasma@ui.ac.id>
2023-11-15 14:39:34 +0700

    commit tutor 8

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from main.forms import ProductForm
 from django.urls import reverse
 from main.models import Product
@@ -118,6 +119,7 @@ def get_product_json(request):
     product_item = Product.objects.all()
     return HttpResponse(serializers.serialize('json', product_item))
 
+@login_required(login_url='/login')
 @csrf_exempt
 def add_product_ajax(request):
     if request.method == 'POST':
@@ -131,3 +133,22 @@ def add_product_ajax(request):
         return HttpResponse(b"CREATED", status=201)
     
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
